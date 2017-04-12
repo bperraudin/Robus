e61ba3347c3fbbcd9e327579042b53bb445053b3
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-04-12 10:58:38 +0200

    Remove all Makefiles.

--- robus/hal/template/mcu.mk
+++
@@ -1,9 +0,0 @@
-#-------------------------------------------------------------------------------
-#       Variant information
-#-------------------------------------------------------------------------------
-
-BOARD = template
-MCU = device_name
-DEVICE_CORE = core_type (AVR, ARM, X86)
-CROSS_COMPILE = "avr-" or ""
-MAINCLOCK = example 120000000ul for 120Mhz
