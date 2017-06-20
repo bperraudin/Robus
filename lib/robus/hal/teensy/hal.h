154346236057f8313a1733dd21bc97a4b795a81a
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2017-06-20 16:36:40 +0200

    Add Hal for teensy.

---
+++ lib/robus/hal/teensy/hal.h
@@ -0,0 +1,17 @@
+#ifndef HAL_ATMEGA10
+#define HAL_ATMEGA10
+
+#include "context.h"
+
+void hal_init(void);
+
+unsigned char hal_transmit(unsigned char *data, unsigned short size);
+
+void hal_delay_ms(int factor);
+
+void set_PTP(branch_t branch);
+void reset_PTP(branch_t branch);
+void reverse_detection(branch_t branch);
+
+
+#endif /* HAL_ATMEGA10 */
