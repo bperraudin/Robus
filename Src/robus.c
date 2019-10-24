7bb2d025c4c1ea3c8bd1b3aec94a95c25b45ea81
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-10-24 11:58:19 +0200

    manage baudrate change for timeout

--- Src/robus.c
+++ Src/robus.c
@@ -126,7 +126,7 @@ unsigned char robus_send_sys(vm_t* vm, msg_t *msg) {
             // ACK needed, change the state of state machine for wait a ACK
             ctx.data_cb = catch_ack;
             volatile int time_out = 0;
-            while (!ctx.ack & (time_out < 60)){
+            while (!ctx.ack & (time_out < (60 * (1000000/ctx.baudrate)))){
                 time_out++;
             }
             status_t status;
